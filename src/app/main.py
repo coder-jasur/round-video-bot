262cfc7bf47a68dee510eab8c44ca38420622b38
Jasur <jasurhikmatilloyev08@gmail.com>
2025-08-03 15:48:53 +0500

    edit

--- src/app/main.py
+++ src/app/main.py
@@ -5,7 +5,7 @@ import os
 import asyncpg
 from aiogram import Dispatcher, Bot
 
-from logs.logger_config import setup_logging
+
 from src.app.common.bot_commands import bot_commands
 from src.app.common.get_db_url import construct_postgresql_url
 from src.app.core.config import Settings
@@ -14,6 +14,8 @@ from src.app.handlers import registrar_routers
 from src.app.middleware import register_middlewares
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(level="INFO")
+
 
 
 async def main():
@@ -28,6 +30,7 @@ async def main():
     async with pool.acquire() as conn:
         await create_database_tables(conn)
 
+
     bot = Bot(token=settings.bot_token)
     await bot_commands(bot, settings)
 
@@ -35,14 +38,11 @@ async def main():
     register_middlewares(dp, settings, pool)
     registrar_routers(dp, settings.admins_ids)
 
-
-
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     try:
-        setup_logging("logs/logger.yml")
         os.makedirs("videos", exist_ok=True)
         asyncio.run(main())
     except Exception as e:
